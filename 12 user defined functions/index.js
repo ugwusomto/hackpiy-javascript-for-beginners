4a82dbcbcdfc26b16a4407d3cd2ad29090b474cf
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2021-09-17 11:30:48 +0100

    Completed Lesson 12

---
+++ 12 user defined functions/index.js
@@ -0,0 +1,24 @@
+function greet($message){
+  alert($message);
+}
+
+function sum($a,$b){
+  let $sum = $a + $b;
+  document.write($sum);
+}
+
+function jumpMessanger(){
+  alert("jump")
+}
+
+greet("Good evening");
+sum(3,5);
+jumpMessanger();
+
+let $name = "name";
+let multiplication = function(){
+  alert("multiplied")
+}
+
+
+multiplication();
