e5405438b507c276c39ed681e97e38268f879dde
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2021-09-17 11:24:16 +0100

    Completed Lesson 9

---
+++ 9 built in method 1/methods.js
@@ -0,0 +1,22 @@
+// alert 
+alert("Welcome to  our page")
+
+// confirm function
+let $answer = confirm("Are you sure you want to delete this data")
+console.log($answer);
+
+// prompt function
+let $answer = prompt("What is your country");
+console.log($answer);
+
+// eval functions
+let $answer = eval("40 + 30 * 24 / 40");
+console.log($answer);
+
+// parseInt function
+let $answer = parseInt("30.56");
+console.log($answer);
+
+// parseFloat function
+let $answer = parseFloat("30.56");
+console.log($answer);
