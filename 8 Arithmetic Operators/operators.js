538edd1500fe55a8badb6348857c3a8f57faee10
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2021-09-17 11:22:11 +0100

    completed lesson 8

---
+++ 8 Arithmetic Operators/operators.js
@@ -0,0 +1,25 @@
+// addition +
+// subtraction -
+// multiplication * 
+// division /
+// modulo %
+
+// addition
+let $sum = 30 + 50;
+
+// subtraction
+let $sub = 100 - 40;
+
+// multiplication
+let $mult = 20 * 30;
+
+//division
+let $div = 40 / 2;
+
+// modulo
+let $mod = 31 % 52;
+
+let $total = (((30 * 40) + (30 - 15)) % 3000) / 20;
+
+
+console.log($total);
