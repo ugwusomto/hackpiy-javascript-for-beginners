f71d5b1146566abfe8cc3ed1a78170179b52bc93
Ugwu Somto <ugwujude42@gmail.com>
2021-09-20 05:36:12 +0100

    completed Lesson 19

---
+++ 19 Array/array.js
@@ -0,0 +1,25 @@
+//1. What is array in Javascript
+//2. Example of array application
+//3. Array Syntax
+//4. Types of arrays
+//5. Why does array indexes start with 0
+//6. Project : Arrays and Loops
+
+
+let foodNames = ["yam","rice","beans"];
+let studentData = [
+    ["James",20,"dark"],
+    ["Mark",23,"dark"]
+];
+
+let food1 = foodNames[0];
+let food2 = foodNames[1];
+studentData[0][0] = "Manly";
+
+
+console.log(foodNames)
+
+foodNames[0] = "fufu";
+
+console.log(foodNames)
+console.log(studentData)
